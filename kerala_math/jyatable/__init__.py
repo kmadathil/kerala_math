b9c1d08d3169144db80c9f1e3b8974e55467d431
Karthik Madathil <karthik.m@atonarp.com>
2021-06-16 15:36:07 -0700

    Added Madhava Jya Table

--- kerala_math/jyatable/__init__.py
+++ kerala_math/jyatable/__init__.py
@@ -10,3 +10,51 @@ def to_mst(f: float):
     fourths, _r = divmod(_r, 60)
     return int(minutes), int(seconds), int(thirds), int(fourths)
 
+def to_float(m, s, t=0, f=0):
+    return m + s/60 + t/3600 + f/216000
+
+
+# Class that implements Minutes, seconds, thirds, fourths
+class MST(object):
+    def __init__(self, minutes, seconds=None, thirds=0, fourths=0):
+        if seconds is not None:
+            self.mst = (minutes, seconds, thirds, fourths)
+        else:
+            self.mst = to_mst(minutes)
+
+    def __add__(self, other):
+        a = to_mst(float(self) + float(other))
+        return MST(*a)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        a = to_mst(float(self) - float(other))
+        return MST(*a)
+
+    def __rsub__(self, other):
+        a = to_mst(float(other) - float(self))
+        return MST(*a)
+
+    def __truediv__(self, other):
+        a = to_mst(float(self) / float(other))
+        return MST(*a)
+    
+    def __mul__(self, other):
+        a = to_mst(float(self) * float(other))
+        return MST(*a)
+
+    def __rmul__(self, other):
+        return self.__mul__(other);
+       
+    def __float__(self):
+        return to_float(*self.mst)
+
+    def __str__(self):
+        return ",".join([str(x) for x in self.mst])
+
+    def __repr__(self):
+        return str(self)
+
+    
